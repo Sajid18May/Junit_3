32c3b6036f06acc432ade80bf1a1cd7acadb39d7
Sajid18May <sajid1852002@gmail.com>
2025-08-22 02:35:56 +0530

    added the project files

---
+++ src/test/java/com/junit_3/Junit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
