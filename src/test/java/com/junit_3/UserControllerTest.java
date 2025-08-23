898c48608bea908b3e2dcb380e91e2f446475f58
Sajid18May <sajid1852002@gmail.com>
2025-08-23 19:32:16 +0530

    added test cases for register

---
+++ src/test/java/com/junit_3/UserControllerTest.java
@@ -0,0 +1,43 @@
+package com.junit_3;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.junit_3.controller.UserController;
+import com.junit_3.service.UserService;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockitoBean
+	private UserService userService;
+	
+	@Test
+	void getUser_Found_Returns200() throws Exception{
+		when(userService.getUserName(1)).thenReturn("Alice");
+		
+		mockMvc.perform(get("/users/1"))
+		.andExpect(status().isOk())
+		.andExpect(content().string("Alice"));
+	}
+	
+	@Test
+	void getUser_NotFound_Returns404() throws Exception{
+		when(userService.getUserName(2)).thenReturn("Unknown User");
+		
+		mockMvc.perform(get("/users/2"))
+		.andExpect(status().isNotFound());
+	}
+	
+}
